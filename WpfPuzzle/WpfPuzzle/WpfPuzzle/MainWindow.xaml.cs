a59c5eb40956c1d0f40267e5fd15b52012393ab5
JuliyaB <juliya24bondarenko@gmail.com>
2020-04-21 20:46:26 +0300

    private void PuzzleItemDrop

--- WpfPuzzle/WpfPuzzle/WpfPuzzle/MainWindow.xaml.cs
+++ WpfPuzzle/WpfPuzzle/WpfPuzzle/MainWindow.xaml.cs
@@ -75,6 +75,83 @@ namespace WpfPuzzle
                 DragDrop.DoDragDrop(cvDragSource, data, DragDropEffects.Move);
             }
         }
+            private void PuzzleItemDrop(object sender, DragEventArgs e)
+        {
+            Canvas destination = sender as Canvas;
+            PuzzlePiece itemTransferred = null;
+            object data = e.Data.GetData(typeof(PuzzlePiece)) as PuzzlePiece;
+            itemTransferred = data as PuzzlePiece;
+
+            Image imageControl = new Image()
+            {
+                Width = destination.Width,
+                Height = destination.Height,
+                Source = itemTransferred.PuzzleImageSource,
+                Stretch = Stretch.UniformToFill
+            };
+
+            if (destination.Children.Count == 0)
+            {
+                destination.Children.Add(imageControl);
+                int indexToUpdate = int.Parse(destination.Tag.ToString());
+                if (itemTransferred.DragFrom == typeof(ListBox))
+                {
+                    this.itemPlacement[indexToUpdate] = itemTransferred;
+                    ((IList)lbDragSource.ItemsSource).Remove(itemTransferred);
+                }
+                else if (itemTransferred.DragFrom == typeof(Canvas))
+                {
+                    int previousIndex = itemPlacement.IndexOf(itemTransferred);
+                    itemPlacement[indexToUpdate] = itemTransferred;
+                    itemPlacement[previousIndex] = emptyItem;
+
+                    Canvas associated = GetAssociatedCanvasByIndex(previousIndex);
+                    associated.Children.Clear();
+                    associated = null;
+                }
+                else
+                {
+                    MessageBox.Show("Dragsource не из listbox или canvas");
+                }
+            }
+            else if (destination.Children.Count > 0)
+            {
+                if (itemTransferred.DragFrom == typeof(ListBox))
+                {
+                    return;
+                }
+                else if (itemTransferred.DragFrom == typeof(Canvas))
+                {
+                    int sourceIndex = itemPlacement.IndexOf(itemTransferred);
+                    int destinationIndex = int.Parse(destination.Tag.ToString());
+
+                    Object buffer = null;
+                    Image image0 = new Image() { Width = destination.Width, Height = destination.Height, Stretch = Stretch.Fill };
+                    image0.Source = itemPlacement[sourceIndex].PuzzleImageSource;
+
+                    Image image1 = new Image() { Width = destination.Width, Height = destination.Height, Stretch = Stretch.Fill };
+                    image1.Source = itemPlacement[destinationIndex].PuzzleImageSource;
+
+                    GetAssociatedCanvasByIndex(sourceIndex).Children.Clear();
+                    GetAssociatedCanvasByIndex(destinationIndex).Children.Clear();
+                    GetAssociatedCanvasByIndex(sourceIndex).Children.Add(image1);
+                    GetAssociatedCanvasByIndex(destinationIndex).Children.Add(image0);
+
+                    image0 = null;
+                    image1 = null;
+
+                    buffer = itemPlacement[sourceIndex];
+
+                    itemPlacement[sourceIndex] = itemPlacement[destinationIndex];
+                    itemPlacement[destinationIndex] = buffer as PuzzlePiece;
+
+                    buffer = null;
+                }
+            }
+            puzzle.OnEdit(EventArgs.Empty);
+            itemTransferred.DragFrom = null;
+        }
+
         }
     }
 }
