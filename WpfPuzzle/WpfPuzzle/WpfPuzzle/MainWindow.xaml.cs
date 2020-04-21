e2049b3b111f8c12b8484af156a92e976e78f4ea
JuliyaB <juliya24bondarenko@gmail.com>
2020-04-21 20:37:20 +0300

    добавлен метод public MainWindow()

--- WpfPuzzle/WpfPuzzle/WpfPuzzle/MainWindow.xaml.cs
+++ WpfPuzzle/WpfPuzzle/WpfPuzzle/MainWindow.xaml.cs
@@ -22,7 +22,32 @@ namespace WpfPuzzle
     {
         public MainWindow()
         {
-            InitializeComponent();
+            Puzzle puzzle = new Puzzle();
+            
+            ObservableCollection<PuzzlePiece> itemPlacement = new ObservableCollection<PuzzlePiece>();
+            
+            PuzzlePiece emptyItem = new PuzzlePiece();
+            
+            ListBox lbDragSource;
+            
+            Canvas cvDragSource;
+            public MainWindow()
+            {
+                InitializeComponent();
+                
+                puzzle.Initialize(1);
+                emptyItem.index = -1;
+                emptyItem.PuzzleImageSource = new BitmapImage();
+                emptyItem.UriString = "";
+                for (int i = 0; i < 9; i++)
+                {
+                    itemPlacement.Add(emptyItem);
+                    
+                    itemPlacement[i].index = i;
+                }
+                puzzleItemList.ItemsSource = puzzle.puzzlePiece;
+                puzzle.Edited += new EventHandler(puzzle_Edited);
+            }
         }
     }
 }
